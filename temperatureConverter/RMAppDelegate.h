ab07ea4374e9b0b831eb225b479e11147954db43
Robert Manson <robmanson@gmail.com>
2013-10-03 11:31:32 -0700

    Initial Commit

---
+++ temperatureConverter/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  temperatureConverter
+//
+//  Created by Robert Manson on 10/3/13.
+//  Copyright (c) 2013 Robert Manson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ temperatureConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  temperatureConverter
+//
+//  Created by Robert Manson on 10/3/13.
+//  Copyright (c) 2013 Robert Manson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMAppDelegate class]));
+    }
+}
